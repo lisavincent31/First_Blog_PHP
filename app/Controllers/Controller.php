3299183c28ff40105866155b688014644018ec03
lisavincent31 <lisa.vincent31150@gmail.com>
2023-05-19 11:30:15 +0200

    create the connection to the database

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,19 +2,35 @@
 
 namespace App\Controllers;
 
+use Database\Connection;
+
 abstract class Controller {
-    
-    public function view(string $path, array $params = null)
+
+    protected $db;
+
+    public function __construct(Connection $db) 
+    {
+        // if(session_status() === PHP_SESSION_NONE) {
+        //     session_start();
+        // }
+        $this->db = $db;
+    }
+
+    // function to return a specific view file
+    protected function view(string $path, array $params = null) 
     {
         ob_start();
         $path = str_replace('.', DIRECTORY_SEPARATOR, $path);
         require VIEWS . $path . '.php';
 
-        if($params) {
-            $params = extract($params);
-        }
-        
         $content = ob_get_clean();
+
         require VIEWS . 'layout.php';
+    }
+
+    // function to get the connexion with the database
+    protected function getDB()
+    {
+        return $this->db;
     }
 }
