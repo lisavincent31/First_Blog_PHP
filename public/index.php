bd820ee14ee74b48b910dd61fb9f59fc7a31f154
lisavincent31 <lisa.vincent31150@gmail.com>
2023-05-19 09:48:14 +0200

    create the router with router and route classes

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php 
 
+use Router\Router; 
+
 require '../vendor/autoload.php';
+
+$router = new Router($_GET['url']);
+
+$router->get('/', 'App\Controllers\HomeController@index');
+
+$router->run();
